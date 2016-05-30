90e54dd7d7a0e427e0ddbf914e9e290dd619ca5b
Andy A <mambetica@hotmail.com>
2016-05-30 16:05:53 +0100

    Make cidr_blocks a list

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_security_group_rule" "sgr" {
   from_port = "${var.from_port}"
   to_port = "${var.to_port}"
   protocol = "${var.protocol}"
-  cidr_blocks = "${var.cidr_blocks}"
+  cidr_blocks = ["${var.cidr_blocks}"]
   security_group_id = "${var.security_group_id}"
   source_security_group_id = "${var.source_security_group_id}"
 }
